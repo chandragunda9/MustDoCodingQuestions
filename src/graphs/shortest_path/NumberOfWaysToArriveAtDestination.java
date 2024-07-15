e77267f9e647b89f563000ffc1455a54ac8e28b1
chandragunda9 <chandragunda1215@gmail.com>
2024-07-15 20:50:48 +0530

    shortestpath problems

---
+++ src/graphs/shortest_path/NumberOfWaysToArriveAtDestination.java
@@ -0,0 +1,9 @@
+package graphs.shortest_path;
+
+import java.util.List;
+
+public class NumberOfWaysToArriveAtDestination {
+    static int countPaths(int n, List<List<Integer>> roads) {
+        return 0;
+    }
+}
